1f683248cd493efc0db2c07f956a55a989e70541
sufiyansoftnox12 <171159389+sufiyansoftnox12@users.noreply.github.com>
2024-08-28 23:41:07 +0500

    future section done!

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import HotspotSection from './components/HotspotSection';
 import CtaSection from './components/CtaSection';
 import FaqsSection from './components/FaqsSection';
 import BudgetSection from './components/BudgetSection';
+import FutureSection from './components/FutureSection';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <FutureSection/>
       <FaqsSection />
       <BudgetSection />
       <CtaSection />
