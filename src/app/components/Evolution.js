ae840b9cf6ab1e04b74837f7152846dc0da142fe
sufiyansoftnox12 <171159389+sufiyansoftnox12@users.noreply.github.com>
2024-08-30 19:44:48 +0500

    evolution-sec-inprocess

--- src/app/components/Evolution.js
+++ src/app/components/Evolution.js
@@ -68,6 +68,165 @@ function Evolution() {
               <div className="col-sm-6"></div>
             </div>
           </div>
+          <div className="phase_a-third-row">
+            <div className="row">
+              <div className="col-sm-6"></div>
+              <div className="col-sm-6">
+                <div className="phase_a-third-col">
+                  <div class="dot-container">
+                    <div class="dot"></div>
+                  </div>
+                  <img
+                    src="/images/specifications.png"
+                    alt="Specifications & Planning"
+                  />
+                  <h3>Estimates & Timeline</h3>
+                  <p>
+                    Using all the information from the specs and designs, we
+                    build a spreadsheet estimating all the tasks involved,
+                    timeline and total cost.
+                  </p>
+
+                  <button type="button" class="whole-btn2">
+                    Show More
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="phase_b">
+          <div className="phase_b-head">
+            <h4>PHASE B</h4>
+          </div>
+          <div className="phase_b-process">
+            <div className="phase_b-first-row">
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="phase_b-first-col">
+                    <div class="dot-container">
+                      <div class="dot"></div>
+                    </div>
+                    <img
+                      src="/images/specifications.png"
+                      alt="Designs, Wireframe & Prototype"
+                    />
+                    <h3>Build</h3>
+                    <p>
+                      Engineers build the software, adding new features, fixing
+                      old features & constantly improving code architecture &
+                      scalability.
+                    </p>
+                    <button type="button" class="whole-btn2">
+                      Show More
+                    </button>
+                  </div>
+                </div>
+                <div className="col-sm-6"></div>
+              </div>
+            </div>
+            <div className="phase_b-second-row">
+              <div className="row">
+                <div className="col-sm-6"></div>
+                <div className="col-sm-6">
+                  <div className="phase_b-second-col">
+                    <div class="dot-container">
+                      <div class="dot"></div>
+                    </div>
+                    <img
+                      src="/images/specifications.png"
+                      alt="Specifications & Planning"
+                    />
+                    <h3>Test</h3>
+                    <p>
+                      QA tests the latest build, identifying any bugs that need
+                      to be fixed or features that need improving.
+                    </p>
+                    <button type="button" class="whole-btn2">
+                      Show More
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="phase_b-third-row">
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="phase_b-third-col">
+                    <div class="dot-container">
+                      <div class="dot"></div>
+                    </div>
+                    <img
+                      src="/images/specifications.png"
+                      alt="Designs, Wireframe & Prototype"
+                    />
+                    <h3>Deploy</h3>
+                    <p>
+                      {" "}
+                      Our deployment engineers release the newest build to the
+                      testing platforms, or live, to the App Store, Google Play
+                      store or website.{" "}
+                    </p>
+                    <button type="button" class="whole-btn2">
+                      Show More
+                    </button>
+                  </div>
+                </div>
+                <div className="col-sm-6"></div>
+              </div>
+            </div>
+            <div className="phase_b-forth-row">
+              <div className="row">
+                <div className="col-sm-6"></div>
+                <div className="col-sm-6">
+                  <div className="phase_b-forth-col">
+                    <div class="dot-container">
+                      <div class="dot"></div>
+                    </div>
+                    <img
+                      src="/images/specifications.png"
+                      alt="Specifications & Planning"
+                    />
+                    <h3>Measure</h3>
+                    <p>
+                      QA tests the latest build, identifying any bugs that need
+                      to be fixed or features that need improving.
+                    </p>
+                    <button type="button" class="whole-btn2">
+                      Show More
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="phase_b-inprocess">
+            <div className="phase_b-fifth-row">
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="phase_b-fifth-col">
+                    <div class="dot-container">
+                      <div class="dot"></div>
+                    </div>
+                    <img
+                      src="/images/specifications.png"
+                      alt="Designs, Wireframe & Prototype"
+                    />
+                    <h3>Maintain</h3>
+                    <p>
+                      Should we exit the software development loop, we have
+                      lightweight plans available for continued, long-term
+                      maintenance.
+                    </p>
+                    <button type="button" class="whole-btn2">
+                      Show More
+                    </button>
+                  </div>
+                </div>
+                <div className="col-sm-6"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
