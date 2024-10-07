932da87b167c8b1ea33bea33bb44eadb39dbed64
Sheryar-code <56033782+Sheryar-code@users.noreply.github.com>
2024-10-07 08:34:31 -0700

    updatedcodelighttheme

--- src/app/app-development/page.js
+++ src/app/app-development/page.js
@@ -8,6 +8,9 @@ import RecentNews from "../components/RecentNews";
 import TestimonialTabs from "../components/TestimonialTabs";
 import MarqueeSlider from "../components/MarqueeSlider";
 import CaseStudy from "../components/CaseStudy";
+import HotspotSection from "../components/HotspotSection";
+import FaqsSection from "../components/FaqsSection";
+import BudgetSection from "../components/BudgetSection";
 
 function page() {
   return (
@@ -24,27 +27,12 @@ function page() {
               <br />
               <br />
               <br />
-              <h3>Android App Development Services</h3>
+              <h3></h3>
             </div>
             <div className="col-md-4"></div>
           </div>
           <hr />
-          <div className="row py-4">
-            <div className="col-md-6 d-flex justify-content-start align-items-center">
-              <h3>Overview</h3>
-            </div>
-            <div className="col-md-6">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen bookIt has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s
-              </p>
-            </div>
-          </div>
+
         </div>
       </div>
 
@@ -182,47 +170,221 @@ function page() {
           </div>
         </div>
       </div>
+      <div className="second-last-section">
+        <div className="container">
+          <div className="row">
+
+            <div className="col-md-6 d-flex align-items-center justify-content-center">
+              <div className="text-section">
+                <h1 className="title-page">discover our history</h1>
+                <p className="title-page-p">Leading with years of experience, Dignite Studios has been centering on the modernization of mobile software specifications to create magnificent web and mobile applications for their valuable clients. Our company of mobile business intelligence has a proven track record of triumphant mobile projects which are illustrious in today’s digital realm.
+                </p>
+                <p className="title-page-p">
+                  We have achieved phenomenal growth in empowering diverse businesses with innovation and contemporary ideas to enliven various blueprints. We have globally earned remarkable endorsement and apprehension in the “INC 5000 list.” Throughout the journey, our unwavering focus has remained on transformation, client satisfaction, and making an indelible impact in the realms of technology and software.</p>
+              </div>
+            </div>
+            <div className="col-md-6 pe-5 py-2">
+              <img
+                src="./images/third-section.png"
+                alt="faqs-sec"
+                className="h-100 w-100"
+              />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
 
       {/* Third Section */}
       <CaseStudy />
-
-      {/* Fourth Section */}
-      {/* <div className="second-trusted">
+      <div className="second-last-section" style={{ backgroundColor: "#FFFBF8", padding: "30px 0px 60px 0px" }}>
         <div className="container">
           <div className="row">
-            <h2 className="text-center">
-              Trusted by Innovators, Loved by Users
-            </h2>
-            <div className="col-md-2 mt-5">
-              <img src="./images/quotation.svg" alt="quotation" />
-            </div>
-            <div className="col-md-6 mt-5"></div>
-            <div className="col-md-4 mt-5">
-              <OneTestimonial />
+            <div className="col-md-9 py-5 d-flex align-items-center justify-content-center">
+              <div className="text-section">
+                <h1 className="title-page">mobile app development services for iOS & ANDROID</h1>
+                <p className="title-page-p">Our software development life cycle empowers companies to functionalize their brand identity by building an effective and powerful mobile app for its users. To create more clicks and produce more revenue we provide a complete mobile solution to make a global recognition of your mobile applications. </p>
+
+              </div>
             </div>
           </div>
-        </div>
-        <div className="container-fluid">
           <div className="row">
-            <div className="col-md-12">
-              <div className="marquee-slider mt-5">
-                <div className="marquee-content">
-                  {duplicatedMarqueeItems.map((item, index) => (
-                    <div className={`item ${item.class}`} key={index}>
-                      <img src={item.src} />
-                    </div>
-                  ))}
-                </div>
+            <div className="col-md-5 d-flex align-items-center justify-content-center">
+              <img
+                src="./images/mobile-app.png"
+                alt="faqs-sec"
+                className="h-100 w-100"
+              />
+            </div>
+
+            <div className="col-md-7 pe-5 py-2">
+              <div className="text-section">
+                <h1 className="title-page-2">Android App Development</h1>
+                <p className="title-page-p-2">Struggling to find an innovative partner for your android mobile app development? Here we offer the best mobile app solutions to propel your business in the world of the mobile stratosphere with our eccentric development approach. Whereas, Android apps are the cornerstone of the transformative development process. </p>
+
+              </div>
+              <div className="text-section">
+                <h1 className="title-page-2">iOS App Development</h1>
+                <p className="title-page-p-2">
+                  Turn your illustrations into an unprecedented model that robust iOS mobile applications in an exclusive manner. From code to launch, the top-notch developers of Dignite Studios will transform your entire mobile app with eminent methodologies of mobile development to make your app version highly functional, optimized, systemized, and scalable on any platform. We take pride in our development efforts to help businesses achieve their rapid ROIs and conversions.
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </div> */}
-      <TestimonialTabs />
+      </div>
 
-      <MarqueeSlider />
+      <div className="second-last-section">
+        <div className="container">
+          <div className="row">
+      
+            <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <img
+                src="./images/line-chart.png"
+                alt="faqs-sec"
+                className="line-image"
+
+              />
+              <div>
+              <div className="text-section-hover">
+                <h1 className="title-page-2">Native Mobile App Development</h1>
+                <p className="title-page-p-2">Our software development life cycle empowers companies to functionalize their brand identity by building an effective and powerful mobile app for its users. </p>
+
+              </div>
+              <div className="text-section-hover">
+                <h1 className="title-page-2">Native Mobile App Development</h1>
+                <p className="title-page-p-2">Our software development life cycle empowers companies to functionalize their brand identity by building an effective and powerful mobile app for its users. </p>
+
+              </div>
+              <div className="text-section-hover">
+                <h1 className="title-page-2">Native Mobile App Development</h1>
+                <p className="title-page-p-2">Our software development life cycle empowers companies to functionalize their brand identity by building an effective and powerful mobile app for its users. </p>
+
+              </div>
+              <div className="text-section-hover">
+                <h1 className="title-page-2">Native Mobile App Development</h1>
+                <p className="title-page-p-2">Our software development life cycle empowers companies to functionalize their brand identity by building an effective and powerful mobile app for its users. </p>
+
+              </div>
+              </div>
+            </div>
+            <div className="col-md-6 pe-5 py-2">
+              <img
+                src="./images/solution.png"
+                alt="faqs-sec"
+                className="h-100 w-100"
+              />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="second-last-section" style={{ backgroundColor: "#FFFBF8", padding: "30px 0px 60px 0px" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-9 py-5 d-flex align-items-center justify-content-center">
+              <div className="text-section">
+               <h1 className="title-page">benefits of working withdignite studios</h1>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 pe-5 py-2">
+            <h1 className="title-page-2">Project Outlook</h1>
+                <p className="title-page-p-2">
+                Cynergy Studios eradicates all risk factors including incomplete business requirements, unrealistic client expectations, and conducts in-depth interviews concerning value-added improvements in the process, resources, and clear workflow to conduct the projects. We aim to meet project dimensions, milestones, and fast development in the given period. We have established hundreds of successful mobile app development projects by promising clients obligations </p>
+            </div>
+
+            <div className="col-md-4 pe-5 py-2">
+              <div className="text-section">
+                <h1 className="title-page-2">Collaboration</h1> 
+                <p>Mobile apps we develop with proper research and interpretation to cater to in-depth project flow. Also, we contribute to collaborate frequently with the client to provide all the updates in the app's development process.Mobile apps we develop with proper research and interpretation to cater to in-depth project flow. Also, we contribute to collaborate frequently with the client to provide all the updates in the app's development process.</p></div>
+              </div>
+              <div className="col-md-4 pe-5 py-2">
+              <div className="text-section">
+                <h1 className="title-page-2">Cost Estimation</h1>
+                <p className="title-page-p-2">
+                Explore a wide range of factors that include creating mobile development effectively and under time constraints with our mobile app developers.Therefore, development costs vary from project to project, however clear and precise project estimation can help to build accurate mobile project planning by covering all its variables. We also analyze various complexities of the apps before moving it further.</p>
+              </div>
+            </div>
+          </div>
+           <div className="row">
+            <div className="col-md-4 pe-5 py-2">
+            <h1 className="title-page-2">Project Outlook</h1>
+                <p className="title-page-p-2">
+                Cynergy Studios eradicates all risk factors including incomplete business requirements, unrealistic client expectations, and conducts in-depth interviews concerning value-added improvements in the process, resources, and clear workflow to conduct the projects. We aim to meet project dimensions, milestones, and fast development in the given period. We have established hundreds of successful mobile app development projects by promising clients obligations </p>
+            </div>
+
+            <div className="col-md-4 pe-5 py-2">
+              <div className="text-section">
+                <h1 className="title-page-2">Collaboration</h1> 
+                <p>Mobile apps we develop with proper research and interpretation to cater to in-depth project flow. Also, we contribute to collaborate frequently with the client to provide all the updates in the app's development process.Mobile apps we develop with proper research and interpretation to cater to in-depth project flow. Also, we contribute to collaborate frequently with the client to provide all the updates in the app's development process.</p></div>
+              </div>
+              <div className="col-md-4 pe-5 py-2">
+              <div className="text-section">
+                <h1 className="title-page-2">Cost Estimation</h1>
+                <p className="title-page-p-2">
+                Explore a wide range of factors that include creating mobile development effectively and under time constraints with our mobile app developers.Therefore, development costs vary from project to project, however clear and precise project estimation can help to build accurate mobile project planning by covering all its variables. We also analyze various complexities of the apps before moving it further.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div className="last-section" style={{ backgroundColor: "#fff", padding: "30px 0px 100px 0px" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 py-5 d-flex align-items-center justify-content-center">
+              <div className="text-section">
+               <h1 className="title-page">optimized process</h1>
+              </div>
+                </div>
+                </div>
+              <img
+                src="./images/banner-bottom.png"
+                alt="faqs-sec"
+                className="h-100 w-100"
+              />
+              </div>
+          
+    </div>
+    <div className="last-section" style={{ backgroundColor: "#fff", padding: "30px 0px 100px 0px" }}>
+      
+          <div className="row">
+            <div className="col-md-12 py-5 d-flex align-items-center justify-content-center">
+              <div className="text-section">
+               <h1 className="title-page">optimized process</h1>
+              </div>
+                </div>
+                </div>
+              <img
+                src="./images/techbanner.png"
+                alt="faqs-sec"
+                className="h-100 w-100"
+              />
+              </div>
+              <div className="case-study py-5 my-5">
+              <div className="row">
+        <div className="col-md-12">
+       <h2>up-to-the-minute</h2>
+          <p>Our software development life cycle empowers companies to functionalize their brand identity by building an effective and powerful mobile app for its users. To create more clicks and produce more revenue we provide a complete mobile solution to make a global recognition of your mobile applications.</p></div>
+        {/* <div className="col-md-2">
+          <button>All Work</button>
+        </div> */}
+      </div>
+      </div>
+      {/* <TestimonialTabs /> */}
+
+      {/* <MarqueeSlider /> */}
       <div className="mt-5">
-        <RecentNews />
+      <FaqsSection />
+      <BudgetSection />
+      {/* <HotspotSection /> */}
+        {/* <RecentNews /> */}
+        
       </div>
       <Footer />
     </>
--- src/app/components/CaseStudy.js
+++ src/app/components/CaseStudy.js
@@ -6,34 +6,35 @@ import "slick-carousel/slick/slick-theme.css";
 
 const CaseStudy = () => {
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />,
   };
 
   return (
-    <div className="case-study container py-5 my-5">
+    <div className="case-study py-5 my-5">
       <div className="row">
-        <div className="col-md-10">
-          <h2>Case Studies</h2>
+        <div className="col-md-12">
+       <h2>mobile apps we develop</h2>
+          <p>Diginite Studios creates top-notch mobile applications and cutting-edge mobile features for their users to clinch their potency and worthiness. Our mobile app services endeavor to build Android and IOS mobile applications that function as per the required needs of the user. Heading with successful mobile projects we fabricate app success in the procreation of unbeatable user interfaces, coding systems, database management, security, and maintenance</p>
         </div>
-        <div className="col-md-2">
+        {/* <div className="col-md-2">
           <button>All Work</button>
-        </div>
+        </div> */}
       </div>
       <div className="row">
         <div className="col-md-12">
           <div className="slider-container">
-            <Slider {...settings}>
-              <div>
+            <Slider  {...settings}>
+              <div style={{margin:"0px 20px",background:"linear-gradient(178.64deg, #F854AB -6.27%, #F33D5A 101.93%)"}}>
                 <img
                   src="./images/blog-1.png"
                   alt="image-1"
-                  className="w-100"
+                  // className="w-100"
                 />
                 <h3>VideoApply</h3>
                 <p>promo website, saas</p>
@@ -69,6 +70,7 @@ const CaseStudy = () => {
           </div>
         </div>
       </div>
+      
     </div>
   );
 };
