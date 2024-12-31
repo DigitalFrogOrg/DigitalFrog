7e3a1924c18a71234ec430f70a070a2c22891b64
AdnanMusil123 <adnan.amnox@gmail.com>
2024-12-31 18:42:40 +0500

    ai page updatation acc to figma

--- src/app/ai-app-development/page.js
+++ src/app/ai-app-development/page.js
@@ -5,42 +5,9 @@ import FaqsSection from "../components/FaqsSection";
 import BudgetSection from "../components/BudgetSection";
 import MainBanner from "../components/MainBanner";
 import IOSAppDesignServices from "../components/IosDesignServices";
-
+import {faqData,aiSoftware,aiBenefits} from './data'
 const Page = () => {
-  const faqData = [
-    {
-      questionId: "01",
-      question: "Who designs Android mobile apps?",
-      answer:
-        "Android mobile apps are designed by a team that typically includes Android app designers, UI/UX specialists, and developers. This collaboration ensures that the app is visually appealing, user-friendly, and functional.",
-    },
-    {
-      questionId: "02",
-      question: "What does an Android mobile app designer do?",
-      answer:
-        "An Android mobile app designer focuses on creating the visual and interactive elements of the app. They ensure the user interface is engaging and intuitive, aligning with user needs and business goals.",
-    },
-    {
-      questionId: "03",
-      question:
-        "What is the difference between an Android app developer and an Android app designer?",
-      answer:
-        "An Android app developer is responsible for coding and building the app’s functionality, while an Android app designer concentrates on the app's aesthetics and user experience. Together, they create a cohesive product.",
-    },
-    {
-      questionId: "04",
-      question:
-        "What is the difference between Android app design vs. iOS app design?",
-      answer:
-        "The primary difference lies in the design guidelines and user interface principles dictated by Google for Android and Apple for iOS. Each platform has distinct design philosophies, affecting layout, navigation, and overall user experience.",
-    },
-    {
-      questionId: "05",
-      question: "How much does it cost to design an Android app?",
-      answer:
-        "The cost of designing an Android app can vary widely, depending on factors such as complexity, features, and the designer's experience. On average, you might expect costs to range from a few thousand to tens of thousands of dollars.",
-    },
-  ];
+
   return (
     <>
       <Header />
@@ -88,95 +55,32 @@ const Page = () => {
           accordingly
         </p>
         <div className="row mt-5">
-          <div className="col-md-4">
-            <div className="AISoftwareDevBox">
-              <img src="./images/IconStore1.png" alt="IconStore1" />
-              <h4>Natural Language Processing</h4>
-              <p>
-                Natural language processing (NLP) solutions are capable of
-                understanding and analyzing written text and speech. This AI
-                development service can be used to build voice assistants,
-                gather data about meaning and emotions, and build a more
-                efficient communication strategy.
+          {aiSoftware.slice(0,3).map((item,i)=>(
+            <div className={`col-md-4 ${i > 0 ?'mgTop25':''}`} key={i}>
+            <div className="AISoftwareDevBox mobileCardPadding">
+              <img src={`/images/${item.imgUrl}`} alt="IconStore1" />
+              <h4 className="cardHeading">{item.heading}</h4>
+              <p className="paragraphText">
+               {item.description}
               </p>
             </div>
           </div>
-          <div className="col-md-4 mgTop25">
-            <div className="AISoftwareDevBox">
-              <img src="./images/IconStore2.png" alt="IconStore2" />
-              <h4>Custom AI applications</h4>
-              <p>
-                Whether you want to analyze medical data, prepare a
-                recommendation list for users, predict prices for sales and
-                marketing strategy, and reach any other of your business goals,
-                our specialists will analyze your requirementsWhether you want
-                to analyze medical data, prepare and reach any other of your
-                business create a perfect custom solution for them.
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 mgTop25">
-            <div className="AISoftwareDevBox">
-              <img src="./images/IconStore3.png" alt="IconStore3" />
-              <h4>Computer Vision</h4>
-              <p>
-                Computer vision works almost like a human eye. It enables a
-                machine to identify, process, recognize, and understand all
-                types of objects in images and videos. Yellow’s AI software
-                developers will help you integrate computer vision into your
-                aComputer vision works almost you integrate computer vision into
-                your application, website, or platform.pplication, website, or
-                platform.
-              </p>
-            </div>
-          </div>
+          ))
+          }
         </div>
         <div className="row mt-4">
-          <div className="col-md-4">
-            <div className="AISoftwareDevBox">
-              <img src="./images/IconStore4.png" alt="IconStore4" />
-              <h4>Data Analytics</h4>
-              <p>
-                The process of structuring and analyzing data can be
-                significantly sped up by implementing artificial intelligence
-                and machine learning algorithms. AI app development will ensure
-                you receive valuable insights and get organized reports with
-                ease.The process of structuring and analyzing data can be
-                significantly sped up by implementing artificial intelligence
-                and machine learning algorithms.
+        {aiSoftware.slice(3,6).map((item,i)=>(
+            <div className={`col-md-4 ${i > 0 ?'mgTop25':''}`} key={i}>
+            <div className="AISoftwareDevBox mobileCardPadding">
+              <img src={`/images/${item.imgUrl}`} alt="IconStore1" />
+              <h4 className="cardHeading">{item.heading}</h4>
+              <p className="paragraphText">
+               {item.description}
               </p>
             </div>
           </div>
-          <div className="col-md-4 mgTop25">
-            <div className="AISoftwareDevBox">
-              <img src="./images/IconStore5.png" alt="IconStore5" />
-              <h4>Neural Networks</h4>
-              <p>
-                Like computer vision mimics the human eye, neural networks mimic
-                the human brain. to notice underlying relationships in data and
-                cLike computer vision mimics the human eye, neural networks
-                mimic the human brain that is trained to notice underlying
-                relationships predictive analysis. Yellow is ready to provide
-                you with this AI software development service and help you build
-                an NN.
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 mgTop25">
-            <div className="AISoftwareDevBox">
-              <img src="./images/IconStore6.png" alt="IconStore6" />
-              <h4>Chatbots</h4>
-              <p>
-                Our artificial intelligence developers are experienced in
-                building AI-based chatbots for websites and mobile apps. A
-                chatbot empowered by AI algorithms can boost your customer
-                service’s productivity and positively affect user
-                experiencewebsites and mobile apps. A chatbot empowered by
-                customer service’s productivity and positively affect user
-                experience.
-              </p>
-            </div>
-          </div>
+          ))
+          }
         </div>
       </div>
 
@@ -186,7 +90,7 @@ const Page = () => {
             Key Applications of AI
             <br /> Technology in Education
           </h2>
-          <p style={{ fontSize: "14px" }}>
+          <p style={{ fontSize: "14px" }} className="paragraphText">
             One of the most significant contributions of AI in education is
             personalized learning. Traditional classrooms often
             <br /> struggle to address the diverse learning needs of each
@@ -203,8 +107,8 @@ const Page = () => {
               />
             </div>
             <div className="col-md-6 footerSection">
-              <h4>Intelligent Tutoring Systems (ITS):</h4>
-              <p>
+              <h4 className="title-page-2">Intelligent Tutoring Systems (ITS):</h4>
+              <p className="paragraphText">
                 AI-based tutors can identify a student's strengths and
                 weaknesses and adjust the curriculum in real-time. This ensures
                 that learners receive content that matches their level of
@@ -213,8 +117,8 @@ const Page = () => {
                 learners receive content that matches their level of
                 understanding, leading to better learning outcomes.
               </p>
-              <h4>Adaptive Learning Platforms:</h4>
-              <p>
+              <h4 className="title-page-2">Adaptive Learning Platforms:</h4>
+              <p className="paragraphText">
                 Platforms like Knewton and Smart Sparrow use machine learning to
                 create personalized learning paths for students. These systems
                 can suggest learning materials, exercises, and quizzes based on
@@ -235,7 +139,7 @@ const Page = () => {
         <div className="row pt-5">
           <div className="col-md-12">
             <h2 className="mainHeadingMobile">Benefits of AI in Education</h2>
-            <p>
+            <p className="paragraphText">
               Our software development life cycle empowers companies to
               functionalize their brand identity by building an effective and
               powerful mobile app for its users. To create more clicks and
@@ -247,119 +151,16 @@ const Page = () => {
 
         <div className="container my-5">
           <div className="row">
-            <div className="col-md-6">
-              <div className="up-to-mints-div">
-                <h4>Strategic Free Consultation</h4>
-                <p>
-                  Curious about how seamlessly Cynergy Studios crafts a
-                  world-class project flow for software? Here’s how we do it:
-                  Our expert consultants work closely with businesses to create
-                  effective strategies that boost conversions and broaden
-                  audience engagement. This approach drives sustainable growth
-                  and long-term success.
+            {aiBenefits.map((item,i)=>(
+              <div className="col-md-6 mb-4" key={i}>
+              <div className="up-to-mints-div mobileCardPadding">
+                <h4 className="cardHeading">{item.heading}</h4>
+                <p className="cardDesc">
+                  {item.description}
                 </p>
               </div>
             </div>
-            <div className="col-md-6 mgTop25">
-              <div className="up-to-mints-div ">
-                <h4>UI/UX Research & Strategy</h4>
-                <p>
-                  Our thorough research process involves interviews, usability
-                  tests, surveys, and analytics to understand user behavior
-                  deeply. Cynergy Studios’ top-tier mobile app design services
-                  adopt the most suitable approach to monitor and refine
-                  consumer insights, ensuring that all aspects of the user's
-                  needs are effectively addressed.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-4">
-            <div className="col-md-6">
-              <div className="up-to-mints-div">
-                <h4>Informative Architectural Layout</h4>
-                <p>
-                  Clear, structured information is the foundation of designing
-                  any user interface, ensuring that apps or websites are
-                  intuitive, easy to navigate, and function seamlessly. As a
-                  leading mobile app design company, Cynergy Studios excels in
-                  conducting comprehensive UX audits to assess and enhance the
-                  effectiveness of digital products, ensuring an exceptional
-                  user experience.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 mgTop25">
-              <div className="up-to-mints-div">
-                <h4>Wireframing</h4>
-                <p>
-                  UX wireframes and designs act as blueprints that visualize and
-                  examine user experiences before moving to the final stage.
-                  Essentially, they provide a simplified version of your product
-                  to ensure operational functions are seamlessly integrated. At
-                  Cynergy Studios, we offer comprehensive mobile app design
-                  services that help projects reach their highest potential,
-                  enabling efficient scaling and strategic growth.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-4">
-            <div className="col-md-6">
-              <div className="up-to-mints-div">
-                <h4>Intuitive User-Interface</h4>
-                <p>
-                  At this stage, the app begins to take its true form, with
-                  structure and design coming together. The exceptional team at
-                  Cynergy Studios delivers top-tier UI/UX design services,
-                  ensuring the product's appearance, behavior, and style align
-                  perfectly with visual element standards. We create
-                  comprehensive style guides, validate every design aspect, and
-                  refine user interface elements to enhance the product’s look
-                  and feel.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 mgTop25">
-              <div className="up-to-mints-div">
-                <h4>Iteration</h4>
-                <p>
-                  During the development process, the mobile app design
-                  undergoes several stages, including testing, reviewing, and
-                  collecting feedback to ensure alignment with the overall
-                  vision, requirements, and brand voice. This collaborative
-                  approach guarantees that our mobile design services meet the
-                  highest standards and fulfill all necessary specifications in
-                  line with mobile competencies.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-4">
-            <div className="col-md-6">
-              <div className="up-to-mints-div">
-                <h4>Prototyping</h4>
-                <p>
-                  In this phase, the app design ideas are refined through
-                  various types of prototyping, allowing the design team to test
-                  and enhance concepts quickly. At Cynergy Studios, we ensure
-                  you receive a highly optimized application tailored to your
-                  specific needs.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 mgTop25" >
-              <div className="up-to-mints-div">
-                <h4>Deployment</h4>
-                <p>
-                  In the final stage, we verify the app's performance and
-                  overall functionality before launching it on the Apple Store
-                  or Google Play. Our mobile app design process prioritizes best
-                  practices in time management and ensures a seamless and secure
-                  deployment procedure.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
